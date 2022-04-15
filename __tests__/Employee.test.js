ca521bc32d6d118d555baee3073448dc60c2808f
rtleib <rtleib@gmail.com>
2022-04-14 23:27:57 -0500

    added employee object and constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,10 @@
 const { TestWatcher } = require("jest")
 const Employee = require("../lib/Employee")
 // object
+test("Can create employee object", () => {
+    const employee = new Employee()
+    expect(typeof(employee)).toBe("object")
+})
 // name
 // id
 // email
