b0d5f612688962235aaac12835fe92be0f194ef5
rtleib <rtleib@gmail.com>
2022-04-15 01:23:58 -0500

    added getRole function in manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,3 +4,8 @@ test("Can set office number info", () => {
     const manager = new Manager("John Smith", "210", "testing@test.com", "officenumber")
     expect(manager.officeNumber).toBe("officenumber")
 })
+// getRole()
+test("We can get the Role of the manager", () => {
+    const manager = new Manager("John Smith", "210", "testing@test.com", "officenumber")
+    expect(manager.getRole()).toBe("manager")
+})
