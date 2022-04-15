7b34dc605942ca61d3522de47ba9486b0ef8dd08
rtleib <rtleib@gmail.com>
2022-04-15 00:35:52 -0500

    added github for engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,6 @@
 const Engineer = require("../lib/Engineer")
+// github
+test("Can set github info", () => {
+    const engineer = new Engineer("John", "210", "testing@test.com", "test")
+    expect(engineer.github).toBe("test")
+})
