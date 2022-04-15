882c830049a255ac8f52e4988b0e770acee65a18
rtleib <rtleib@gmail.com>
2022-04-14 23:35:48 -0500

    added name function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,6 +6,11 @@ test("Can create employee object", () => {
     expect(typeof(employee)).toBe("object")
 })
 // name
+test("Can set the name of the employee", () =>{
+    const employee = new Employee("John Smith", "210", "testing@test.com")
+    console.log(employee)
+    expect(employee.name).toBe("John Smith")
+})
 // id
 // email
 // getName()
