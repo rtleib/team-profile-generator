281afc9b440019d821fa7cb17e88a5ece8375b37
rtleib <rtleib@gmail.com>
2022-04-15 00:43:15 -0500

    added getGithub function to engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,3 +4,9 @@ test("Can set github info", () => {
     const engineer = new Engineer("John", "210", "testing@test.com", "test")
     expect(engineer.github).toBe("test")
 })
+// getGithub()
+test("We can get the GitHub of the engineer", () => {
+    const engineer = new Engineer("John Smith", "210", "testing@test.com", "test")
+    expect(engineer.getGithub()).toBe("test")
+})
+// getRole()
