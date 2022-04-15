c30f4600d6279bd0971fec906fbcbfa54c88262e
rtleib <rtleib@gmail.com>
2022-04-15 01:20:42 -0500

    added officenumber function to manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,6 @@
 const Manager = require("../lib/Manager")
+// officeNumber
+test("Can set office number info", () => {
+    const manager = new Manager("John Smith", "210", "testing@test.com", "officenumber")
+    expect(manager.officeNumber).toBe("officenumber")
+})
