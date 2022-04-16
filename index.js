82914d1a3dbf9ea81b4bc733978ebdd62c73a9fd
rtleib <rtleib@gmail.com>
2022-04-16 13:43:25 -0500

    added id, email, office number questions for mananger section

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ inquirer
     {
       type: 'input',
       name: 'managerName',
-      message: "What is the mangager's name",
+      message: "What is the mangager's name?",
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is the mangager's id?",
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: "What is the mangager's email?",
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "What is the mangager's office number?",
     },
   ])
