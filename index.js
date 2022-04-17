d8bf8d48c28b76749409a8add200de2d27ead1f7
rtleib <rtleib@gmail.com>
2022-04-16 22:15:46 -0500

    fixed createIntern function

--- index.js
+++ index.js
@@ -47,8 +47,56 @@ inquirer
         } else {
           writeToFile('index.html', html(team));
         }
-      })
+      });
   })
+  function createIntern() {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'internName',
+        message: "What is the intern's name?",
+      },
+      {
+          type: 'input',
+          name: 'internId',
+          message: "What is the intern's id?",
+      },
+      {
+          type: 'input',
+          name: 'internEmail',
+          message: "What is the intern's email?",
+      },
+      {
+          type: 'input',
+          name: 'internSchool',
+          message: "What school did the intern went to?",
+      },
+    ])
+    .then((data) => {
+        const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+        team.push(newIntern);
+        console.log(newIntern)
+        inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'choice',
+            message: 'Which member would you like to add?',
+            choices: ['Engineer', 'Intern', 'None'],
+          },
+        ])
+        .then((data) => {
+          if (data.choice === 'Engineer') {
+            createEngineer();
+          } else if (data.choice === 'Intern') {
+            createIntern();
+          } else {
+            writeToFile('index.html', html(team));
+          }
+        })
+    })
+  }  
   function createEngineer() {
     inquirer
       .prompt([
@@ -96,52 +144,5 @@ inquirer
             }
           })
       })
-      function createIntern() {
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            name: 'internName',
-            message: "What is the intern's name?",
-          },
-          {
-              type: 'input',
-              name: 'internId',
-              message: "What is the intern's id?",
-          },
-          {
-              type: 'input',
-              name: 'internEmail',
-              message: "What is the intern's email?",
-          },
-          {
-              type: 'input',
-              name: 'internSchool',
-              message: "What school did the intern went to?",
-          },
-        ])
-        .then((data) => {
-            const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
-            team.push(newIntern);
-            console.log(newIntern)
-            inquirer
-            .prompt([
-              {
-                type: 'list',
-                name: 'choice',
-                message: 'Which member would you like to add?',
-                choices: ['Engineer', 'Intern', 'None'],
-              },
-            ])
-            .then((data) => {
-              if (data.choice === 'Engineer') {
-                createEngineer();
-              } else if (data.choice === 'Intern') {
-                createIntern();
-              } else {
-                writeToFile('index.html', html(team));
-              }
-            })
-        })
-      }  
+
   }
